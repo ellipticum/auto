f7de09a60cab8221dcf55bc5f83888e047c4906b
ellipticum <ellipticum@outlook.com>
2024-05-15 22:36:56 +0600

    feat: redirect unauthorized

--- src/widgets/car-info/UI/index.tsx
+++ src/widgets/car-info/UI/index.tsx
@@ -1,25 +1,27 @@
-// CarInfo.js
-
 'use client'
 
 import React, { useEffect, useState } from 'react'
 import styles from './styles.module.scss'
-import { ICar } from '@/shared/store/auth'
+import useAuthStore, { ICar } from '@/shared/store/auth'
 import Container from '@/shared/UI/layouts/container'
 import useEmblaCarousel from 'embla-carousel-react'
 import cn from 'classnames'
 import useOrderWindowStore from '@/widgets/order-window/model/store'
+import { useRouter } from 'next/navigation'
 
 interface Props {
     car: ICar | undefined
 }
 
 const CarInfo = ({ car }: Props) => {
+    const { isAuth } = useAuthStore()
     const { setIsOrderWindowHidden, setCar } = useOrderWindowStore()
 
     const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, align: 'center' })
 
     const [selectedIndex, setSelectedIndex] = useState(0)
+
+    const router = useRouter()
 
     if (!car) {
         return null
@@ -106,6 +108,12 @@ const CarInfo = ({ car }: Props) => {
                             <button
                                 className={styles.button}
                                 onClick={() => {
+                                    if (!isAuth) {
+                                        router.push('/')
+
+                                        return
+                                    }
+
                                     setCar(car)
                                     setIsOrderWindowHidden(false)
                                 }}
